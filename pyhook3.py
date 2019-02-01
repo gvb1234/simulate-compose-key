727596c98ebb1a6f8762767a99cdbc5baa2a34ef
Guillaume van Baalen <gvb@linux-nfmr.site>
2019-02-01 18:41:40 +0100

    main library file
    change author

--- pyhook3.py
+++ pyhook3.py
@@ -1,6 +1,39 @@
 #!/usr/bin/python
 
-# main library file
+# pyxhook -- an extension to emulate some of the PyHook library on linux.
+#
+#    Copyright (C) 2008 Tim Alexander <dragonfyre13@gmail.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#    Thanks to Alex Badea <vamposdecampos@gmail.com> for writing the Record
+#    demo for the xlib libraries. It helped me immensely working with these
+#    in this library.
+#
+#    Thanks to the python-xlib team. This wouldn't have been possible without
+#    your code.
+#    
+#    This requires: 
+#    at least python-xlib 1.4
+#    xwindows must have the "record" extension present, and active.
+#    
+#    This file has now been somewhat extensively modified by 
+#    Daniel Folkinshteyn <nanotube@users.sf.net>
+#    So if there are any bugs, they are probably my fault. :)
+#
+#	  and modified by gvb1234 as well ;-)
  
 #import sys
 #import os
